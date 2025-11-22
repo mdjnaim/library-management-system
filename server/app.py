9b2abe07c5e7f5697465d0f06b38ba0e3c39bea1
tanvir <tanviralam451@gmail.com>
2025-11-22 12:05:55 +0600

    Comment out unused borrow return import in app.py and format root response for better readability

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
+# file: server/app.py
 from fastapi import FastAPI
 from .api_book_management import app as book_management # relative import
 from .api_user_management import app as user_management # relative import
-from .api_borrow_return import app as borrow_return # relative import
+# from .api_borrow_return import app as borrow_return # relative import
 
 app = FastAPI(
     title="Library Management System",
@@ -11,8 +12,10 @@ app = FastAPI(
 
 app.mount("/book", book_management)
 app.mount("/user", user_management)
-app.mount("/borrows", borrow_return)
 
 @app.get("/", response_model=dict, status_code=200)
 async def root():
-    return {"title": app.title, "version": app.version, "description": app.description}
+    return {"title": app.title, 
+            "version": app.version, 
+            "description": app.description
+        }
